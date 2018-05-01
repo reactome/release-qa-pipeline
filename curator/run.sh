aba41751cb26290ab29e7bf36ec4262ae7b1ce95
FredLoney <loneyf@ohsu.edu>
2018-04-30 19:20:00 -0700

    Add --help option.

--- curator/run.sh
+++ curator/run.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env bash
+
+function display_help () {
+    echo "Usage: $0"
+    exit 0
+}
+
+# Parse the command line options.
+while true; do
+  case "$1" in
+    -* ) display_help; shift ;;
+    * ) break ;;
+  esac
+done
+
 rm -f QA_Output/*
 java -Xmx8G -jar curator-with-dependencies.jar org.gk.qualityCheck.CommandLineRunner
