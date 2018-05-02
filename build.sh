dc2d372aac1043d55529eee83f8a3992b6131849
FredLoney <loneyf@ohsu.edu>
2018-05-02 13:06:40 -0700

    Build a single graph importer jar file with embedded dependencies.

--- build.sh
+++ build.sh
@@ -4,9 +4,7 @@
 (cd curator; ant -f create-jar.xml)
 
 # Graph.
-cp -f ../graph-importer/target/BatchImporter.jar graph
-[ -d graph/lib ] || mkdir graph/lib
-cp -fr ../graph-importer/target/lib/* graph/lib
+(cd graph; ant -f create-jar.xml)
 cp -f ../graph-qa/target/graph-qa-jar-with-dependencies.jar \
    graph/graph-qa-with-dependencies.jar
 
@@ -22,5 +20,5 @@ cp -fr ../release-qa/target/release-qa-*jar-with-dependencies.jar \
 # Assemble the distro.
 mkdir -p dist
 tar czf dist/release-qa-pipeline.tar.gz \
-    README.md */*.sh */*.jar */lib \
+    README.md */*.sh */*.jar \
     curator/QA_SkipList curator/resources/*.txt curator/resources/*.properties
